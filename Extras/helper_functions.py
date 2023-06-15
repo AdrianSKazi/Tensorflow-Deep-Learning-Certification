3740e2e798c169bf99d4be3dbb35307105e30b36
Adrian S.K <64697142+AdrianSKazi@users.noreply.github.com>
2023-06-15 22:54:32 +0200

    Update helper_functions.py
    Adding compare_histories function

--- Extras/helper_functions.py
+++ Extras/helper_functions.py
@@ -286,3 +286,42 @@ def calculate_results(y_true, y_pred):
                   "recall": model_recall,
                   "f1": model_f1}
   return model_results
+
+# Let's create a function to compare training histories of 5 feature extraction model and 5 of additionaly fine tuned model
+
+def compare_histories(original_history, new_history, initial_epochs = 5):
+  """
+  Compares two TensorFlow History objects.
+  """
+
+  # Get original history measurements
+  acc = original_history.history['accuracy']
+  loss = original_history.history['loss']
+
+  val_acc = original_history.history['val_accuracy']
+  val_loss = original_history.history['val_loss']
+
+  # Combine original history
+  total_acc = acc + new_history.history['accuracy']
+  total_loss = loss + new_history.history['loss']
+
+  total_val_acc = val_acc + new_history.history['val_accuracy']
+  total_val_loss = val_loss + new_history.history['val_loss']
+
+  # Make plot for accuracy
+  plt.figure(figsize = (8, 8))
+  plt.subplot(2, 1, 1)
+  plt.plot(total_acc, label = 'Training Accuracy')
+  plt.plot(total_val_acc, label = 'Val Accuracy')
+  plt.plot([initial_epochs-1, initial_epochs-1], plt.ylim(), label = 'Start Fine Tuning') # We're subtracting 1 from initial epochs because we have 0 index
+  plt.legend(loc = 'lower right')
+  plt.title('Training and Validation Accuracy')
+
+  # Make plot for loss
+  plt.figure(figsize = (8, 8))
+  plt.subplot(2, 1, 2)
+  plt.plot(total_loss, label = 'Training Loss')
+  plt.plot(total_val_loss, label = 'Val Loss')
+  plt.plot([initial_epochs-1, initial_epochs-1], plt.ylim(), label = 'Start Fine Tuning') # We're subtracting 1 from initial epochs because we have 0 index
+  plt.legend(loc = 'upper right')
+  plt.title('Training and Validation Loss')
